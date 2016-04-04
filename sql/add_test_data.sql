3ca0be311430a1dc98ec603111918e5c1f7a3610
Samuli Rouvinen <samrouvi@dhcp-asv-112.helsinki.fi>
2016-04-04 23:08:42 +0300

    lisätty SQL-lauseet ja päivitetty hieman html-malleja

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,11 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Account (username, password, isMod) VALUES ('pullis', '1234', true);
+INSERT INTO Account (username, password) VALUES ('eriksi', '4321');
+
+INSERT INTO Group (name) VALUES ('modit');
+
+INSERT INTO AccountGroup (accoID, groupID) VALUES (0, 0);
+
+INSERT INTO Topic (groupID) VALUES (0);
+
+INSERT INTO Message (topicID, accoID, content) VALUES (0, 0, 'Testi viesti');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,35 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Account(
+
+	id SERIAL PRIMARY KEY,
+	username varchar(30) NOT NULL,
+	password varchar(30) NOT NULL,
+	isMod boolean NOT NULL DEFAULT FALSE
+);
+
+CREATE TABLE Group(
+
+	id SERIAL PRIMARY KEY,
+	name varchar(30) NOT NULL
+);
+
+CREATE TABLE AccountGroup(
+
+	accoID INTEGER REFERENCES Account(id),
+	groupID INTEGER REFERENCES Group(id)
+);
+
+CREATE TABLE Topic(
+
+	id SERIAL PRIMARY KEY,
+	groupID INTEGER REFERENCES Group(id)
+);
+
+CREATE TABLE Message(
+
+	id SERIAL PRIMARY KEY,
+	postTime datetime DEFAULT now(),
+	topicID INTEGER REFERENCES Topic(id),
+	accoID INTEGER REFERENCES Account(id),
+	content varchar(400) NOT NULL
+);
